74098f0c53275a13df2dd8c8e5e9f9d927e831ae
heejun <nohj91@naver.com>
2017-02-13 17:30:02 +0900

    last

--- TestGit/src/TestGit.java
+++ TestGit/src/TestGit.java
@@ -2,16 +2,6 @@
 public class TestGit {
 
 	public TestGit(){
-		System.out.println("dasdasd");
-		System.out.println("dddddddddd");
-		System.out.println("dddddd");
-		System.out.println("�뼵�젣易� �릺�젮�굹..");
-		System.out.println("dd");
-		System.out.println("�샇�꽍 : �븞�뀞?");
-		System.out.println("dd");
-		System.out.println("�뼵�젣�뀎易�...�릺由с뀞�굹....2");
-		System.out.println("dd");
-		System.out.println("�깶�엫�엫�꽮");
-		System.out.println("뷁");
+		System.out.println("마지막 수정");
 	}
 }
