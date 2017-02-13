b2fd44fd74b3dcbc7f8a74c19ec89ef229229335
songyi <song2yeyong@naver.com>
2017-02-13 17:27:42 +0900

    ㅋㅋㅋㅋㅋㅋㅋㅋ

--- TestGit/src/TestGit.java
+++ TestGit/src/TestGit.java
@@ -5,13 +5,12 @@ public class TestGit {
 		System.out.println("dasdasd");
 		System.out.println("dddddddddd");
 		System.out.println("dddddd");
-		System.out.println("�뼵�젣易� �릺�젮�굹..");
+		System.out.println("언제쯤 되려나..");
 		System.out.println("dd");
-		System.out.println("�샇�꽍 : �븞�뀞?");
+		System.out.println("호석 : 안녕?");
 		System.out.println("dd");
-		System.out.println("�뼵�젣�뀎易�...�릺由с뀞�굹....2");
+		System.out.println("언제ㅉ쯤...되리ㅕ나....2");
 		System.out.println("dd");
-		System.out.println("�깶�엫�엫�꽮");
-		System.out.println("뷁");
+		System.out.println("샤랄랄ㄹ");
 	}
 }
